a1048c3747a63f74a5892b169d098c56e0a816d8
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-10-28 19:35:09 +0700

    remove unused file and code

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, ipsa a.</p>
+        </div>
+    )
+}
+
+export default index
