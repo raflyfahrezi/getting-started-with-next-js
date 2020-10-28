4446289408d6b79df1aab6d2ff978854489a94a3
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-10-28 19:52:27 +0700

    edit head

--- src/components/head/head.jsx
+++ src/components/head/head.jsx
@@ -4,7 +4,17 @@ import Head from 'next/head'
 const head = () => {
     return (
         <Head>
-            
+            <meta charset='utf-8' />
+            <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+            <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+            <meta name='description' content='Nextjs with PWA' />
+            <meta name='keywords' content='PWA' />
+            <title>Next.js with PWA</title>
+
+            <link rel="manifest" href="/manifest.json" />
+            <link href='/icons/icons.png' rel='icon' type='image/png' sizes='512x512' />
+            <link rel="apple-touch-icon" href="/icons/icons.png"></link>
+            <meta name="theme-color" content="#000000"/>
         </Head>
     )
 }
